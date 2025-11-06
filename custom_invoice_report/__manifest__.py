d3eb2e5fff4eb4806db7ded0c0152dc0371df235
khizar217naat-alt <khizar217naat@gmail.com>
2025-11-06 15:44:51 +0500

    Add files via upload

---
+++ custom_invoice_report/__manifest__.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "custom_invoice_report",
+
+    'summary': "Short (1 phrase/line) summary of the module's purpose",
+
+    'description': """
+Long description of module's purpose
+    """,
+
+    'author': "My Company",
+    'website': "https://www.yourcompany.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base' , 'account'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/custom_header.xml',
+        'views/custom_footer.xml',
+        'views/report_action.xml',
+        'views/report.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [],
+}
+
